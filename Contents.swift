508e14bb9741e88656ce10fb4fd8f23a87a9346e
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-10-10 15:47:01 +0900

    chapter3

---
+++ Contents.swift
@@ -0,0 +1,41 @@
+print (1 + 1)
+print (5 - 3)
+print (3 * 3)
+print (6 / 2)
+print(7 % 3)
+
+//地球の面積
+print(4 / 3 * 3.14 * 6378 * 6378 * 6378)
+
+//変数と定数
+var x = 3
+print(x * x)
+
+let tax = 1.1
+print(100 * tax)
+
+//繰り返し文
+for x in 1...9{
+    print(3 * x) //３の段
+    print(4 * x) //４の段
+}
+
+var sum = 0
+for n in 1...100{
+    sum = n + sum
+}
+print(sum)
+
+
+var batteryRemaining = 18
+print("バッテリーは残り\(batteryRemaining)%です")
+
+var bodyTemp = 36.0
+bodyTemp = 36.5
+
+
+
+
+
+
+
